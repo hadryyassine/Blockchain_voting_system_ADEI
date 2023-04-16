ea62b32911e2c1991ae85de2b51fa58c93217b4e
hadryyassine <hadryyassine@gmail.com>
2023-04-16 02:12:53 +0000

    Initial Set up of The backend with Spring Boot

---
+++ BackendofADEIVotechain/src/main/java/com/Votechainbackend/BackendofADEIVotechain/BackendofAdeiVotechainApplication.java
@@ -0,0 +1,13 @@
+package com.Votechainbackend.BackendofADEIVotechain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendofAdeiVotechainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendofAdeiVotechainApplication.class, args);
+	}
+
+}
---
+++ BackendofADEIVotechain/src/test/java/com/Votechainbackend/BackendofADEIVotechain/BackendofAdeiVotechainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Votechainbackend.BackendofADEIVotechain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendofAdeiVotechainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
