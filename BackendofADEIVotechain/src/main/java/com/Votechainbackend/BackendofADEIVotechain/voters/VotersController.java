1bce991a742feedb1b316090d31fa65661296080
Salma GARMOUTI <101300774+GarmoutiSalma@users.noreply.github.com>
2023-05-01 21:42:16 +0100

    voters structure

--- BackendofADEIVotechain/src/main/java/com/Votechainbackend/BackendofADEIVotechain/voters/VotersController.java
+++ BackendofADEIVotechain/src/main/java/com/Votechainbackend/BackendofADEIVotechain/voters/VotersController.java
@@ -3,6 +3,9 @@ package com.Votechainbackend.BackendofADEIVotechain.voters;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import com.Votechainbackend.BackendofADEIVotechain.voters.VotersModel;
+import java.lang.String;
 
 @RestController // This is a REST API Controller
 @RequestMapping(path = "api/v1/voters")
@@ -17,7 +20,7 @@ public class VotersController {
                         "john.smith@gmail.com",
                         "password",
                         "1234567890"
-                ));
+                )).toString();
     }
 
 }
