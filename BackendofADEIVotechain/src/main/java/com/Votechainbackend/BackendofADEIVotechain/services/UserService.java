a1d366c800bd9d3e9787a9057f00c2fc6307aa27
hadryyassine <hadryyassine@gmail.com>
2023-05-08 02:30:51 +0100

    Add some methods in the UserService interface

--- BackendofADEIVotechain/src/main/java/com/Votechainbackend/BackendofADEIVotechain/services/UserService.java
+++ BackendofADEIVotechain/src/main/java/com/Votechainbackend/BackendofADEIVotechain/services/UserService.java
@@ -1,8 +1,14 @@
 package com.Votechainbackend.BackendofADEIVotechain.services;
 
+import com.Votechainbackend.BackendofADEIVotechain.entities.Committee;
 import com.Votechainbackend.BackendofADEIVotechain.entities.User;
 
+import java.util.List;
+
 public interface UserService {
+    User CreatUser(User user);
+    User findById(Long id);
+    List<User> findAll();
     User registerUser(User user);
     User findByEmailAddress(String emailAddress);
 }
