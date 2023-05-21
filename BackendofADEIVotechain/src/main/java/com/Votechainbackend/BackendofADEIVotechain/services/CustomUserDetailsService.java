381bbee84f9df89d34993d4e89bd4c7ed7a14426
hadryyassine <hadryyassine@gmail.com>
2023-05-21 02:20:45 +0100

    add React services for auth

--- BackendofADEIVotechain/src/main/java/com/Votechainbackend/BackendofADEIVotechain/services/CustomUserDetailsService.java
+++
@@ -1,7 +0,0 @@
-package com.Votechainbackend.BackendofADEIVotechain.services;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface CustomUserDetailsService extends UserDetailsService {
-}
-
