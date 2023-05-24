177263db47cfa63463a9d407476317ff193ad1c8
hadryyassine <hadryyassine@gmail.com>
2023-05-24 20:09:28 +0100

    Make the unit tests

--- BackendofADEIVotechain/src/main/java/com/Votechainbackend/BackendofADEIVotechain/dto/CandidateRequest.java
+++ BackendofADEIVotechain/src/main/java/com/Votechainbackend/BackendofADEIVotechain/dto/CandidateRequest.java
@@ -4,6 +4,10 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 public class CandidateRequest {
+    public CandidateRequest(String name) {
+        this.name = name;
+    }
+
     @NotBlank
     @Size(max = 40)
     private String name;
@@ -15,4 +19,6 @@ public class CandidateRequest {
     public void setName(String name) {
         this.name = name;
     }
+
+
 }
